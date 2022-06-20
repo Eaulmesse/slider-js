305f54721613fdc5cf442045774b9708f41bcda7
eaulmesse <theo.guerin01120@gmail.com>
2022-06-20 16:09:21 +0200

    feat: working

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const previousBtn = document.getElementById("previous");
 const nextBtn = document.getElementById("next");
 
+
+
 let slider = document.getElementById("slider");
 let img = ["./imgs/img1.jpg", "./imgs/img2.jpg", "./imgs/img3.jpg"]
 
-previousBtn.addEventListener("click", ()=> {
-    slider.style.background = `url('${img.value = }')`;
+var i = 0;
+slider.style.background = `url('${img[i]}')`;
+
+
+nextBtn.addEventListener("click", ()=> {
     
+
+    if (i < img.length-1) {
+        i++;
+        slider.style.background = `url('${img[i]}')`;
+    }
+
+    console.log(i);
+    console.log( slider.style.background);
+})
+
+
+previousBtn.addEventListener("click", ()=> {
+    
+   
+    if (i > 0) {
+        i--;
+        slider.style.background = `url('${img[i]}')`;
+    }
+
+
+    console.log(i); 
+    console.log( slider.style.background);
 })
